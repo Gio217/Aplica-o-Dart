0916dc9888ebb8f8a8abc6565389b397ec03f847
Giovanna Gurdiano <70184032+Gio217@users.noreply.github.com>
2022-05-05 12:12:54 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "StateLess",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: "Stateful",
+      theme: ThemeData(primarySwatch: Colors.red),
       home: Home(),
     );
   }
@@ -22,27 +22,60 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String nome = "";
+
+  @override
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // retorna um objeto do tipo Dialog
+        return AlertDialog(
+          title: Text("Seu nome é "),
+          content: Text(nome),
+          actions: [
+            TextButton(
+                child: Text('Fechar'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                })
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        title: Text("Estudando Statefull"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: TextButton(
-        style: TextButton.styleFrom(
-          backgroundColor: Colors.blue,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Estudando Statefull"),
+          centerTitle: true,
         ),
-        child: Text(
-          'FlatButton Ativo',
-          style: TextStyle(
-            color: Colors.white,
+        body: Center(
+          child: Container(
+            width: 300,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextField(
+                  decoration: const InputDecoration(border: OutlineInputBorder(), labelText: "Coloque seu nome"),
+                  onChanged: (String str) {
+                    setState(() {
+                      nome = str;
+                    });
+                  },
+                ),
+                TextButton(
+                    onPressed: () {
+                      _showDialog(context);
+                    },
+                    child: const Text("Clique aqui"))
+              ],
+            ),
           ),
         ),
-        onPressed: () {},
       ),
-    )));
+    );
   }
 }
